7d40724ece3bfb2ea12d63a59c482242a76c675a
mrkindy <admin@mrkindy.com>
2024-10-09 23:17:51 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Process;
 
 Route::get('/', function () {
-
-    $result = Process::run('ssh mrkindy@157.173.101.221');
-    return $result->output();
-
+    return "Hello Envoy example";
 });
